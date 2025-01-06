9ef197499265bde5a7391a6ac3b8f4c2ef0819b6
Yuvraj Singh Jat <yuvrajsingh1008jat@gmail.com>
2025-01-06 16:16:10 +0530

    Changes by Yuvraj

--- modals/user/grievance.modals.js
+++ modals/user/grievance.modals.js
@@ -1,3 +1,32 @@
-const express= require("express");
-const mongoose = require("mongoose")
-const bodyparser= require("body-parser")
+const mongoose = require("mongoose");
+const Schema= mongoose.Schema();
+
+//Define Grievance schema
+const grievanceSchema= new Schema(
+    {
+        grievanceid:{
+            type: Number,
+            required: true,
+            unique: true
+        },
+        subject:{
+            type: String,
+            required: true
+        },
+        description:{
+            type:String,
+            required: true
+        },
+        scholarno:{
+            type: Number,
+            required: true,
+            unique: true
+        },
+        proof:{
+            type: String
+        }
+    }
+);
+
+//Define Grievance Model
+const Grievance= mongoose.model('Grievance', grievanceSchema);
