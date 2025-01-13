f61fbba6cd586d1b3d28dcd8e3c53fe418ec1522
Bhavika <chandrawat.bhavika@gmail.com>
2025-01-13 15:27:38 +0530

    Admin Controller Understand

--- controllers/admin/auth.controllers.js
+++ controllers/admin/auth.controllers.js
@@ -7,12 +7,50 @@ const options = {
 }
 
 const adminSignin = asyncHandler(async ( req,res )=>{
+    let email = req.body.email;
+    let password = req.body.password;
 
+    const adminToLogin= await Admin.findOne({
+        email: email,
+        password: password
+    }).select("-password")
+    if(!adminToLogin){
+        throw new ApiError(404,"Admin not found!")
+    }
+
+    const {accessToken,refreshToken}= await createAccessAndRefreshToken(findAdminToLogin._id)
+    const findLoggedAdmin = Admin.findId(findAdminToLogin._id).select("-password -refreshToken")
+    if(!findLoggedAdmin){
+        throw new ApiError(404,"Admin not forund after token generation")
+    } 
+    return res
+    .status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken.options)
+    .json(
+        new ApiResponse(200,findLoggedAdmin,"Admin logged in successfully")
+    )
 } )
 
 
 const adminLogout = asyncHandler( async(req,res)=>{
-
+    console.log(req.verificationOfAdmin._id)
+    await Admin.findByIdAndUpdate(
+        req.verificationOfAdmin._id,
+        {
+            $set:{
+                refreshToken: undefined
+            },
+            {
+                new: true
+            }
+        }
+    )
+    return res
+    .status(200)
+    .clearCookie(accessToken,options)
+    .clearCookie(refreshToken,options)
+    .send(200,{},"User logout properly")
 })
 
 
