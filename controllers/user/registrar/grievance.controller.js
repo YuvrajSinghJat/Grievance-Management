5a78f5797282798dbf2b854399d03027888ef1d5
Mohit Chouhan <mohit.chouhan.71099@gmail.com>
2025-07-31 15:07:10 +0530

    registrar viewgrievance updated

---
+++ controllers/user/registrar/grievance.controller.js
@@ -0,0 +1,19 @@
+//optional 
+const { Grievance } = require("../../modals/user/grievance.modals");
+const { ApiResponse } = require("../../utility/ApiResponse");
+const { ApiError } = require("../../utility/ApiError");
+const { asyncHandler } = require("../../utility/asyncHandler");
+
+// Get All Grievances
+const getAllGrievances = async (req, res) => {
+  try {
+    const grievances = await Grievance.find({}).populate("studentId", "name email");
+    res.status(200).json(grievances);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to fetch grievances", error: err.message });
+  }
+};
+
+module.exports = {
+  getAllGrievances,
+};
--- routes/registrar/registrar.routes.js
+++ routes/registrar/registrar.routes.js
@@ -3,11 +3,18 @@ const router = express.Router();
 
 // Correct relative path
 const registrarController = require('../../controllers/user/registrar/registrar.controllers.js');
+// const grievanceController = require('../../controllers/user/registrar/grievance.controller.js');
+const viewgrievancecontroller = require('../../controllers/user/registrar/view.controller.js')
 
 // View forwarded reports from Dosa
 router.get('/view-forwarded-reports', registrarController.getReports);
 
 // Update final status of grievance
 router.post('/update-grievance-status', registrarController.updateGrievanceStatus);
+
+// router.post('/registrargrievance', grievanceController.getAllGrievances);
+
+router.post('/allgrievanceregistrar', viewgrievancecontroller.viewAllGrievances);
+router.post('/viewSingleGrievances', viewgrievancecontroller.viewSingleGrievances);
 
 module.exports = router;
